6c1678328d27ca2c81c4d69e974679dd0d28f47f
unknown <isabelasdeveloper@gmail.com>
2025-12-05 13:53:18 -0300

    projeto finalizado

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,21 @@
 import "../styles/main.scss";
+
+function themes() {
+  const btnColor = document.querySelectorAll(".color-btn");
+
+  btnColor.forEach((btn) => {
+    btn.addEventListener("click", (e) => {
+      e.preventDefault();
+    });
+  });
+
+  const btnThemes = document.querySelectorAll(".card-theme");
+
+  btnThemes.forEach((btn) => {
+    btn.addEventListener("click", (e) => {
+      e.preventDefault();
+    });
+  });
+}
+
+themes();
